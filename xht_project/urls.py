e5e64d16e780281136566542417de49b9af0bb87
YirDev <apjair97@gmail.com>
2020-08-19 23:58:43 -0500

    trying with workers to validate the comunication WSS

--- xht_project/urls.py
+++ xht_project/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('operations/', include('operations.urls')),
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
 ]
