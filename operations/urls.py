2170df904fa5947748eb5e24774f4a66f80e1bac
YirDev <apjair97@gmail.com>
2020-08-19 23:58:21 -0500

    trying with workers to validate the comunication WSS

--- operations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views as operations_views
-
-urlpatterns = [
-    path('', operations_views.index, name='index'),
-     path('<str:room_name>/', operations_views.room, name='room'),
-]
