55f12cab1f367e6bb5eff3dd5522e46788eb72f6
Jair Aguilar <apjair97@gmail.com>
2020-08-25 18:50:58 -0500

    manual handling the request argument error

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-def chat_room(request, label):
+def chat_room(request):
     # If the room with the given label doesn't exist, automatically create it
     # upon first visit (a la etherpad).
-    room, created = Room.objects.get_or_create(label=label)
+    
+    room, created = Room.objects.get_or_create(label='testroom')
 
     # We want to show the last 50 messages, ordered most-recent-last
     messages = reversed(room.messages.order_by('-timestamp')[:50])
