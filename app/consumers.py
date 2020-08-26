146a985ddd9a035704af6d36876e7217a653b4e0
Jair Aguilar <apjair97@gmail.com>
2020-08-26 08:34:01 -0500

    added json depencie to fix consumer issue

--- app/consumers.py
+++ app/consumers.py
@@ -2,10 +2,13 @@ from channels import Group
 from channels.sessions import channel_session
 from .models import Room
 import json
+import re
+
 
 @channel_session
 def ws_connect(message):
-    prefix, label = 'testroom'
+    prefix= 'testroom'
+    label = 'testroom'
     room = Room.objects.get(label='testroom')
     Group('chat-' + label).add(message.reply_channel)
     message.channel_session['room'] = 'testroom'
