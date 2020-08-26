a4cb1ceb20b74e9a1c34bc9b2ba2f8e7657b186f
Jair Aguilar <apjair97@gmail.com>
2020-08-25 20:50:04 -0500

    trying to fix JS bug

--- static/chat.js
+++ static/chat.js
@@ -1,23 +1,34 @@
-
+  
+$(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
-    console.log(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname)
     
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
-        $('#chat').append('<tr>' 
-            + '<td>' + data.timestamp + '</td>' 
-            + '<td>' + data.handle + '</td>'
-            + '<td>' + data.message + ' </td>'
-        + '</tr>');
+        var chat = $("#chat")
+        var ele = $('<tr></tr>')
+
+        ele.append(
+            $("<td></td>").text(data.timestamp)
+        )
+        ele.append(
+            $("<td></td>").text(data.handle)
+        )
+        ele.append(
+            $("<td></td>").text(data.message)
+        )
+        
+        chat.append(ele)
     };
 
-    $('#chatform').on('submit', function(event) {
+    $("#chatform").on("submit", function(event) {
         var message = {
             handle: $('#handle').val(),
             message: $('#message').val(),
         }
-        chat_socket.send(JSON.stringify(message));
+        chatsock.send(JSON.stringify(message));
+        $("#message").val('').focus();
         return false;
     });
+});
