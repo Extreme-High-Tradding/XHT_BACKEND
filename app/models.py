99401c46282a80493a886787c78c40008bbf5eb2
Jair Aguilar <apjair97@gmail.com>
2020-08-26 10:41:37 -0500

    added costume models to handling transaction data

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+# Testing room models
+
 class Room(models.Model):
     name = models.TextField()
     label = models.SlugField(unique=True)
@@ -14,6 +16,27 @@ class Message(models.Model):
     price = models.TextField(null=True,blank=True)
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
 
-class Test(models.Model):
-    name = models.TextField()
-    label = models.SlugField(unique=True)
+# XHT models
+
+class Money(models.Model):
+    actual_money = models.DecimalField(max_digits=19, decimal_places=10)
+    active1_average_price_date = models.DecimalField(max_digits=19, decimal_places=10)
+    active2_average_price_date = models.DecimalField(max_digits=19, decimal_places=10)
+    active3_average_price_date = models.DecimalField(max_digits=19, decimal_places=10)
+    
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Transacctions(models.Model):
+    
+    buy_price_opened = models.DecimalField(max_digits=19, decimal_places=10)
+    sell_price_closed = models.DecimalField(max_digits=19, decimal_places=10)
+    amount_actives = models.IntegerField()
+    date = models.DateTimeField(default=timezone.now)
+    active_id = models.CharField(max_length=50)
