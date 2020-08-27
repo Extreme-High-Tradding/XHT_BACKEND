d18db06b7764c84508cac2e6ff37196012a1b8f0
Yair Mendoza <yairmendo@gmail.com>
2020-08-27 01:11:33 -0500

    Classifying transaction and asset

--- app/consumers.py
+++ app/consumers.py
@@ -19,10 +19,35 @@ def ws_receive(message):
     label = 'testroom'
     room = Room.objects.get(label='testroom')
     data = json.loads(message['text'])
-    m = Transactions.objects.create(user_id=data['user_id'], price_opened =data['price'], 
-                                    price_closed=data['price_closed'], 
-                                    amount_actives=data['amount'],
-                                    operation_type=data['operation_type'], )
+    m = Transactions.objects.create(user_id = data['user_id'], opening_price = data['price'], 
+                                    closing_price = data['closing_price'],
+                                    asset_id = data['asset_id'] 
+                                    amount_assets = data['amount'],
+                                    operation_type = data['operation_type'],
+                                    operation_status = data['operation_status'])
+    #Classifying transaction
+    if m.operation_type == False:
+        Transactions.object.create(user_id = m.user_id, opening_price = m.opening_price,
+                                    amount_assets = m.amount_assets,
+                                    asset_id = m.asset_id)
+        #Modify users balance
+        user_balance = Financial.objects.get(user_id = m.user_id)
+        #try:look for error type and aply try catch function
+        if user_balance.asset_id == 'tesla':
+            user_balance.active1_amount += m.amount_assets
+            user_balance.balance -= m.opening_price
+
+        elif user_balance.asset_id == 'petroleo':
+            user_balance.active2_amount += m.amount_assets
+            user_balance.balance -= m.opening_price
+        
+        elif user_balance.asset_id == 'bitcoin':
+            user_balance.active3_amount += m.amount_assets
+            user_balance.balance -= m.opening_price
+        #Catch: look for error type and aply try catch function
+
+        
+
     Group('chat-'+label).send({'text': json.dumps(m.content)})
 
 
@@ -31,5 +56,13 @@ def ws_receive(message):
 def ws_disconnect(message):
     label = message.channel_session['room']
     Group('chat-'+label).discard(message.reply_channel)
+
+
+
+#Additional functions
+def average(asset_id):
+    list_asset = Transactions.objects.filter(asset_id = asset_id,
+                                            operation_status = True,
+                                            operation_type = False)
 
 
--- app/models.py
+++ app/models.py
@@ -21,22 +21,22 @@ class Message(models.Model):
 
 class Financial(models.Model):
     user_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    actual_money = models.DecimalField(blank = False, max_digits=19, decimal_places=6 )
-    active1_average_price_date = models.DecimalField(blank = True, max_digits=19, decimal_places=10)
-    active2_average_price_date = models.DecimalField(blank = True,max_digits=19, decimal_places=10)
-    active3_average_price_date = models.DecimalField(blank = True,max_digits=19, decimal_places=10)
-    active1_amount = models.DecimalField(blank = True, max_digits=19, decimal_places=10)
-    active2_amount = models.DecimalField(blank = True, max_digits=19, decimal_places=10)
-    active3_amount = models.DecimalField(blank = True, max_digits=19, decimal_places=10)
+    balance = models.DecimalField(default = 100000.000000, max_digits=19, decimal_places=6 )
+    active1_average_price = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
+    active2_average_price = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
+    active3_average_price = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
+    active1_amount = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
+    active2_amount = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
+    active3_amount = models.DecimalField(default = 0, blank = True, max_digits=19, decimal_places=10)
 
 
 class Transacctions(models.Model):
     transaction_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    price_opened = models.DecimalField(max_digits=19, decimal_places=6)
-    price_closed = models.DecimalField(blank = True, null = True, max_digits=19, decimal_places=6)
-    amount_actives = models.IntegerField()
-    date = models.DateTimeField(default=timezone.now)
-    active_id = models.CharField(max_length=50)
-    operation_type = models.BooleanField(default= False)
-    operation_status = models.BooleanField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    opening_price = models.DecimalField(max_digits=19, decimal_places=6)
+    closing_price = models.DecimalField(default = 0, blank = True, null = True, max_digits=19, decimal_places=6)
+    amount_assets = models.IntegerField()
+    date = models.DateTimeField(default = timezone.now)
+    asset_id = models.CharField(max_length = 50)# asset_id = 'tesla', 'petroleo', 'bitcoin'
+    operation_type = models.BooleanField(default = False)# False= = 'Buy', True = 'Sell' 
+    operation_status = models.BooleanField(default = False)# False = open , False = 'close'
