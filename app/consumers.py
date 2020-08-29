db5acf9238a3867bb9ebaad1d840b812ee983620
Jair Aguilar <apjair97@gmail.com>
2020-08-29 15:17:19 -0500

    modified Transaction instancing adding tuble syntax to user instance

--- app/consumers.py
+++ app/consumers.py
@@ -25,7 +25,7 @@ def ws_receive(message):
     #here goes code 1
     user = Financial.objects.get_or_create(user_id_id=int(data['user']))
 
-    m = Transactions.objects.create(user_id = user.user_id_id,
+    m = Transactions.objects.create(user_id = user[0],
                                     opening_price = 200, 
                                     closing_price = 200,
                                     asset_id = 1, 
