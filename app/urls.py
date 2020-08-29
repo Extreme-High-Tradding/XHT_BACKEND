a794313c28100bc05e956b26e8b6853aa52a3b70
Jair Aguilar <apjair97@gmail.com>
2020-08-28 19:29:24 -0500

    added admin site to hadle the data

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url('transactions', views.transactions, name='transactions'), 
+    url('transactions', views.transactions, name='transactions'),
+    path('admin/', admin.site.urls),
 ]
