a2941592dec975226314ef25404c2e05debedd4b
Jair Aguilar <apjair97@gmail.com>
2020-08-29 15:10:19 -0500

    modified Transaction instancing adding PK attrib to user var

--- app/consumers.py
+++ app/consumers.py
@@ -25,7 +25,7 @@ def ws_receive(message):
     #here goes code 1
     user = Financial.objects.get_or_create(user_id_id=int(data['user']))
 
-    m = Transactions.objects.create(user_id = user.pk,
+    m = Transactions.objects.create(user_id = user,
                                     opening_price = 200, 
                                     closing_price = 200,
                                     asset_id = 1, 
