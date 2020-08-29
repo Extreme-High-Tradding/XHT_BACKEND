ff8ec8aac47c1d5a71d2ff332f6857aad8da25b1
YirDev <apjair97@gmail.com>
2020-08-29 04:50:51 -0500

    modified testhtml to hadle the error

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from .models import Room
+from .models import Room, Transactions
 from django.shortcuts import render, redirect
 
 def transactions(request):
@@ -6,11 +6,11 @@ def transactions(request):
     # upon first visit (a la etherpad).
     
     room, created = Room.objects.get_or_create(label='testroom')
-
+    trans = Transactions.objects.all()
     # We want to show the last 50 messages, ordered most-recent-last
     messages = reversed(room.messages.order_by('-timestamp')[:50])
 
     return render(request, "chat/room.html", {
         'room': room,
-        'messages': messages,
+        'messages': trans,
     })
