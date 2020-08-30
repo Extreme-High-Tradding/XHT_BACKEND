cb564738bb69b6732b099824a8ebbbcdb2534c79
Yair Mendoza <yairmendo@gmail.com>
2020-08-29 19:25:05 -0500

    testing buy function v1

--- app/consumers.py
+++ app/consumers.py
@@ -22,24 +22,24 @@ def ws_receive(message):
     label = 'testroom'
     room = Room.objects.get(label='testroom')
     data = json.loads(message['text'])
-    # user_striped = data['user'].strip("'")
-    #here goes code 1
-    user = Financial.objects.get_or_create(user_id_id=int(data['user_id']))
+    # # user_striped = data['user'].strip("'")
+    # #here goes code 1
+    # user = Financial.objects.get_or_create(user_id_id=int(data['user_id']))
 
-    m = Transactions.objects.create(user_id = user[0],
-                                    opening_price = float(data['price']), 
-                                    closing_price = float(data['price']),
-                                    asset_id = data['asset_id'], 
-                                    amount_assets = float(data['amount_assets']),
-                                    operation_type = (data['operation_type'] != 'False'),      #data['operation_type'],  False= = 'Buy', True = 'Sell' 
-                                    operation_status = (data['operation_status'] != 'False'))   #data['operation_status'])  False = open , True = 'close'
-    m.save()
-    # till here code 1
+    # m = Transactions.objects.create(user_id = user[0],
+    #                                 opening_price = float(data['price']), 
+    #                                 closing_price = float(data['price']),
+    #                                 asset_id = data['asset_id'],#1,2,3
+    #                                 amount_assets = float(data['amount_assets']),
+    #                                 operation_type = (data['operation_type'] != 'False'),      #data['operation_type'],  False= = 'Buy', True = 'Sell' 
+    #                                 operation_status = (data['operation_status'] != 'False'))   #data['operation_status'])  False = open , True = 'close'
+    # m.save()
+    # # till here code 1
     serialized_obj = serializers.serialize('json', [ m, ])
     Group('chat-'+label).send({'text': serialized_obj })
     
-    """                                    
-    #Classifying transaction
+                                       
+"""     #Classifying transaction
     if m.operation_type == False:
         Transactions.object.create(user_id = m.user_id, opening_price = m.opening_price,
                                     amount_assets = m.amount_assets,
@@ -59,36 +59,40 @@ def ws_receive(message):
             user_balance.active3_amount += m.amount_assets
             user_balance.balance -= m.opening_price
         #Catch: look for error type and aply try catch function
-        """
-    # #code 1 {
-    # # Buy operation
-    # user_balance = Financial.objects.get(user_id = data['user'])
-    # #if user does not have enough credit, user can not buy assets
-    # if (data['operation_type']== False) & (user_balance.balance >= data['price']):
-    #     #creating transaction row
-    #     m = Transactions.object.create(user_id = data['user'],
-    #                                 opening_price = data['price'],
-    #                                 amount_assets = data['amount'],
-    #                                  operation_type = data['operation_type'],
-    #                                 asset_id = data['asset'])
-    #     #m.save()
-    #     #Modify users balance
-    #     user_balance = Financial.objects.get(user_id = data['user'])
-    #     #try:look for error type and aply try catch function
-    #     if user_balance.asset_id == data['asset']:
-    #         user_balance.active1_amount += data['amount']
-    #         user_balance.balance -= data['price']
+      """  
+    #code 1 {
+    # Buy operation
+    user_balance = Financial.objects.get_or_create(user_id_id=int(data['user_id']))# produccion get(user_id=data[user_id])
+    #if user does not have enough credit, user can not buy assets
+    if (data['operation_type']== 'False') & (user_balance.balance >= float(data['price'])):
+        #creating transaction row
+        m = Transactions.object.create(user_id = user_balance[0],
+                                    opening_price = float(data['price']),#float()
+                                    amount_assets = float(data['amount']),
+                                     operation_type = False,
+                                    asset_id = data['asset'])
+        m.save()
+        #Modify users balance
+        #try:look for error type and aply try catch function
+        if '1' == data['asset']:
+            user_balance.active1_amount += data['amount']
+            user_balance.balance -= data['price']
 
-    #     elif user_balance.asset_id == data['asset']:
-    #         user_balance.active2_amount += data['amount']
-    #         user_balance.balance -= data['price']
+        elif '2' == data['asset']:
+            user_balance.active2_amount += data['amount']
+            user_balance.balance -= data['price']
         
-    #     elif user_balance.asset_id == data['asset']:
-    #         user_balance.active3_amount += data['amount']
-    #         user_balance.balance -= data['price']
-    #     #m.save()
-    # else:
-    #     return print('User does not have enough credit')
+        elif '3' == data['asset']:
+            user_balance.active3_amount += float(data['amount'])
+            user_balance.balance -= data['price']
+        #m.save()
+    else:
+        return print('User does not have enough credit or asset does not exist')#raise
+
+    transaction = serializers.serialize('json', [ m, ])
+    balance = serializers.serialize('json', [ user_balance, ])
+    Group('chat-'+label).send({'text': transaction })
+    Group('chat-'+label).send({'text': balance })
 
     # # Sell function, open transaction
     # #if user does not have enough assets, user can not open a sell movement. with the following lines
@@ -137,7 +141,8 @@ def ws_receive(message):
     #     #m.save()
     #     average()
     # else:
-    #     return print('User ')
+    #     return print('User ')# en lugar de return raise
+    #     raise #leyenda del error 
     #     #Catch: look for error type and aply try catch function 
     # # #code 1   }
 
